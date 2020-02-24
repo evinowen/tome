3549ca2f99a3c84b32cef04115cbd8857bcb0240
Evin Owen <github@evinowen.net>
2020-02-24 14:05:26 -0800

    Create Context Menu service

---
+++ src/components/ContextMenuService.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-menu
+      v-bind:value="value"
+      v-on:input="$emit('input', $event)"
+      :position-x="position_x"
+      :position-y="position_y"
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="item.action"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+  import { remote } from 'electron';
+
+  export default {
+    props: {
+      value: { type: Boolean, default: false },
+      items: { type: Array },
+      position_x: { type: Number, default: 0 },
+      position_y: { type: Number, default: 0 },
+
+    },
+
+  }
+
+</script>
+
