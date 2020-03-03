b8038a5921ea35e5a78bc6639476b512adebe783
Evin Owen <github@evinowen.net>
2020-03-03 08:12:13 -0800

    Disable camelcase, option quote-props lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    'quote-props': ['error', 'consistent'],
+    'camelcase': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
