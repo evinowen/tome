b760bca54e3f9ebb603f3e13d08eb991e83e47f4
Evin Owen <github@evinowen.net>
2020-03-16 13:14:21 -0700

    Update Jest to create junit.xml report for CircleCI

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
     '!**/node_modules/**'
-  ]
-
+  ],
+  reporters: [ 'default', 'jest-junit' ]
 }
