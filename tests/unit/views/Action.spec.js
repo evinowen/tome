2651740408bfad386148ac1b9b0e009646204e08
Evin Owen <github@evinowen.net>
2020-06-13 20:26:54 -0700

    Add tests for Action view

---
+++ tests/unit/views/Action.spec.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+import ActionView from '@/views/Action.vue'
+
+Vue.use(Vuetify)
+
+jest.mock('@/store', () => ({
+  state: {}
+}))
+
+describe('Action.vue', () => {
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  const factory = assemble(ActionView, () => ({ vuetify }))
+
+  it('is able to be mocked and prepared for testing', () => {
+    const wrapper = factory.wrap()
+
+    expect(wrapper).not.toBeNull()
+  })
+})
