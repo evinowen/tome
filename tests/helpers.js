c4680bff40bfcf00b39b4f7518603b505e688c93
Evin Owen <github@evinowen.net>
2020-06-13 20:25:26 -0700

    Create new testing helper functionality

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,22 +1,65 @@
 import { createLocalVue, mount } from '@vue/test-utils'
 
-global.factory = (object, prepare, defaults) => {
-  const { context, use } = prepare()
-
-  return (props) => {
-    const localVue = createLocalVue()
-
-    if (use && use.localVue) {
-      for (item of use.localVue) {
-        localVue.use(item)
-      }
+global.respond = (routable) => {
+  const _routable = routable || []
+  const _mock = jest.fn(async (type, callback) => {
+    if (_routable.indexOf(type) < 0) {
+      return
     }
+
+    await callback()
+  })
+
+  return { routable: _routable, mock: _mock }
+}
+
+global.assemble = (object, defaults) => {
+  const factory = {
+    trap: false,
+    component: {
+      context: null,
+      hook: null
+    }
+  }
+
+  factory.context = (context) => {
+    factory.component.context = context
+    return factory
+  }
+
+  factory.hook = (hook) => {
+    factory.component.hook = hook
+    return factory
+  }
+
+  factory.make = () => {
+    factory.trap = true
+
+    if (factory.component.context) {
+      factory.context = factory.component.context()
+    }
+
+    factory.localVue = createLocalVue()
+
+    if (factory.component.hook) {
+      factory.component.hook(factory)
+    }
+
+    return factory
+  }
+
+  factory.wrap = (props) => {
+    if (!factory.trap) {
+      factory.make()
+    }
+
+    factory.trap = false
 
     return mount(
       object,
       {
-        localVue,
-        ...(context || {}),
+        localVue: factory.localVue,
+        ...(factory.context || {}),
         propsData: {
           ...(defaults || {}),
           ...(props || {})
@@ -25,4 +68,5 @@ global.factory = (object, prepare, defaults) => {
     )
   }
 
+  return factory
 }
