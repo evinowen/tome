b3541612290dfd498d71e735ad8da447413a7cae
Evin Owen <github@evinowen.net>
2020-06-13 20:26:29 -0700

    Add tests for Empty view

---
+++ tests/unit/views/Empty.spec.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+import EmptyView from '@/views/Empty.vue'
+
+Vue.use(Vuetify)
+
+jest.mock('@/store', () => ({
+  state: {}
+}))
+
+describe('Empty.vue', () => {
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  const factory = assemble(EmptyView)
+    .context(() => ({ vuetify }))
+
+  it('is able to be mocked and prepared for testing', () => {
+    const wrapper = factory.wrap()
+
+    expect(wrapper).not.toBeNull()
+  })
+})
