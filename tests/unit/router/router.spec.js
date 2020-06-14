139133b448e230fd38073b6ce1ca67701ce1fd6f
Evin Owen <github@evinowen.net>
2020-06-13 20:38:02 -0700

    Add tests for router

---
+++ tests/unit/router/router.spec.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import router from '@/router'
+
+import { createLocalVue } from '@vue/test-utils'
+
+let localVue
+
+describe('src/router/index.js', () => {
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(router)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('is able to be mocked and prepared for testing', async () => {
+    router.push({ name: '/', params: {}})
+
+    await localVue.nextTick()
+  })
+})
