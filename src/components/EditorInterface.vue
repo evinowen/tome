b6d6024ff7975c1c9a3754dd92a451d0f389418c
Evin Owen <github@evinowen.net>
2020-08-30 21:04:25 -0700

    Update layout search format and updates in editor interface

--- src/components/EditorInterface.vue
+++ src/components/EditorInterface.vue
@@ -14,14 +14,14 @@
       <scrolly class="foo" :style="{ width: '100%', height: '100%' }">
         <scrolly-viewport>
 
-          <div v-show="!edit">
+          <div v-show="!edit" class="fill-height">
             <div v-show="content" style="height: 100%; padding: 0px;" >
               <div id="editor-interface-rendered" ref="rendered" v-html="rendered" class="pa-2" />
             </div>
             <empty-view v-if="!content" />
           </div>
 
-          <div v-show="edit">
+          <div v-show="edit" class="fill-height">
             <!-- COMMIT WINDOW -->
             <commit-view v-if="commit" @close="$emit('commit:close')" />
 
@@ -29,10 +29,10 @@
             <push-view v-else-if="push" @close="$emit('push:close')" />
 
             <!-- OPEN FILE WINDOW -->
-            <codemirror ref="editor" v-else-if="content" :value="content" style="height: 100%;" @input=save />
+            <codemirror ref="editor" v-show="active" :value="content" style="height: 100%;" @input=save />
 
             <!-- ACTION OR ERROR MENUS -->
-            <div v-else class="full_size">
+            <div v-show="!active" class="full_size">
               <empty-view>{{ error }}</empty-view>
             </div>
           </div>
@@ -47,6 +47,10 @@
 </template>
 
 <style>
+.fill-height {
+  height: 100%;
+}
+
 .splitter-paneL,
 .splitter-paneR {
   overflow: hidden;
@@ -68,8 +72,8 @@
 
 .CodeMirror {
   border: 1px solid #eee;
-  height: auto !important;
-  width: auto !important;
+  min-height: 100% !important;
+  min-width: 100% !important;
   overflow: hidden;
 }
 
@@ -111,62 +115,80 @@ export default {
     explore: function () {
       return !(this.commit || this.push)
     },
+    active: function () {
+      return store.state.files.active
+    },
     content: function () {
       return store.state.files.content || ''
     },
     rendered: function () {
       return marked(this.content)
     },
-    search: function () {
-      console.log('search', Object.assign({}, store))
-      return store.state.search.query
-    }
+    query: () => store.state.search.query
   },
   watch: {
-    search: function (value) {
-      const query = new RegExp(value.replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&'), 'gi')
-
-      this.mark.unmark()
-      this.mark.markRegExp(
-        query,
-        {
-          className: 'highlight-rendered'
-        }
-      )
-
-      if (!this.$refs.editor) {
-        console.log('no editor!', value)
-        return
-      }
-
-      const cm = this.$refs.editor.codemirror
-
-      if (this.overlay) {
-        cm.removeOverlay(this.overlay, true)
-      }
-
-      this.overlay = {
-        token: (stream) => {
-          query.lastIndex = stream.pos
-          const match = query.exec(stream.string)
-          if (match && match.index === stream.pos) {
-            stream.pos += match[0].length || 1
-            return 'searching'
-          } else if (match) {
-            stream.pos = match.index
-          } else {
-            stream.skipToEnd()
-          }
-        }
-      }
-
-      cm.addOverlay(this.overlay, true)
-
-      console.log('codemirror', cm, Object.assign({}, cm))
+    query: function () {
+      this.search()
+    },
+    active: function () {
+      this.search()
+    },
+    edit: function () {
+      this.search()
+    },
+    content: function () {
+      this.search()
+    },
+    rendered: function () {
+      this.search()
     }
   },
   methods: {
-    save: async (content) => store.dispatch('files/save', { content })
+    save: async (content) => store.dispatch('files/save', { content }),
+    search: async function () {
+      const query = new RegExp(String(this.query).replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&'), 'gi')
+
+      if (this.edit) {
+        if (!this.$refs.editor) {
+          console.log('no editor!', this.query)
+          return
+        }
+
+        const cm = this.$refs.editor.codemirror
+
+        if (this.overlay) {
+          cm.removeOverlay(this.overlay, true)
+        }
+
+        this.overlay = {
+          token: (stream) => {
+            query.lastIndex = stream.pos
+            const match = query.exec(stream.string)
+            if (match && match.index === stream.pos) {
+              stream.pos += match[0].length || 1
+              return 'searching'
+            } else if (match) {
+              stream.pos = match.index
+            } else {
+              stream.skipToEnd()
+            }
+          }
+        }
+
+        cm.addOverlay(this.overlay, true)
+
+        console.log('codemirror', cm, Object.assign({}, cm))
+      } else {
+        this.mark.unmark()
+        this.mark.markRegExp(
+          query,
+          {
+            className: 'highlight-rendered',
+            done: total => store.dispatch('search/visible', { total, target: null })
+          }
+        )
+      }
+    }
   },
   components: {
     Scrolly,
