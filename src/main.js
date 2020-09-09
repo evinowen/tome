e38de06ceddbe349024bbbaed2b72213cb190d4c
Evin Owen <github@evinowen.net>
2020-09-09 10:12:48 -0700

    Update EditorInterface scrollbars to match other components better

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import SplitPane from 'vue-splitpane'
 import VueCodemirror from 'vue-codemirror'
 import 'codemirror/mode/markdown/markdown.js'
 import 'codemirror/lib/codemirror.css'
+import 'codemirror/addon/scroll/simplescrollbars.css'
+import 'codemirror/addon/scroll/simplescrollbars.js'
 import 'codemirror/addon/search/searchcursor.js'
 
 Vue.component('split-pane', SplitPane)
@@ -19,7 +21,8 @@ Vue.use(VueCodemirror, {
     mode: 'text/x-markdown',
     theme: 'base16-dark',
     lineNumbers: true,
-    line: true
+    line: true,
+    scrollbarStyle: 'overlay'
   }
 })
 
