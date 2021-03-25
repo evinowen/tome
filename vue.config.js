0506aa3b88ec20d07cadf5b6b862f598ccc0e3b2
Evin Owen <github@evinowen.net>
2021-03-25 14:53:46 -0700

    Disable npmRebuild in electron-builder

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
+        npmRebuild: false,
         win: {
           icon: './assets/icon/tome.ico'
         }
