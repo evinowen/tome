fa8570cec7ba47f41217351806d4d8169df1d37e
Evin Owen <github@evinowen.net>
2021-03-25 14:51:34 -0700

    Fixed build process icon loader

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        win: {
+          icon: './assets/icon/tome.ico'
+        }
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
