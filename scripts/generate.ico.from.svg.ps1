a1026ee54fdbb681a2bed61d2d8e8454a439b709
Evin Owen <git@evinowen.net>
2022-07-16 16:59:56 -0700

    Update ico generation for exact 256x256 requirement on Linux

--- scripts/generate.ico.from.svg.ps1
+++ scripts/generate.ico.from.svg.ps1
@@ -1,6 +1,6 @@
 $Root = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 magick $Root\..\assets\icon\tome.png `
-  -define icon:auto-resize=16,48,256 `
+  -define icon:auto-resize=256 `
   -background white -transparent white `
   -compress zip $Root\..\assets\icon\tome.ico
