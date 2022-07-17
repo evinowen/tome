0be716189c7159664b12894165ecd0e1e02bcadd
Evin Owen <git@evinowen.net>
2022-07-16 17:57:02 -0700

    Create new png icon for linux build

--- scripts/generate.ico.from.svg.ps1
+++ scripts/generate.ico.from.svg.ps1
@@ -6,6 +6,9 @@ magick $Root\..\assets\icon\tome.png `
   -compress zip $Root\..\build\icon.ico
 
 magick $Root\..\assets\icon\tome.png `
+  -resize 256x256 $Root\..\build\icon.png
+
+magick $Root\..\assets\icon\tome.png `
   -define icon:auto-resize=32,48,64,128,256 `
   -background white -transparent white `
   -compress zip $Root\..\assets\icon\tome.ico
