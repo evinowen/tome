0878241037b36e285a37884617b31348a7572fea
Evin Owen <git@evinowen.net>
2022-07-16 18:54:06 -0700

    Update icon generation and assignment in package.json

--- scripts/generate.ico.from.svg.ps1
+++ scripts/generate.ico.from.svg.ps1
@@ -1,14 +1,19 @@
 $Root = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
-magick $Root\..\assets\icon\tome.png `
+magick convert -background none -resize 1024x1024 $Root\..\assets\icon\tome.svg $Root\..\assets\icon\tome.1024.png
+
+magick $Root\..\assets\icon\tome.1024.png `
   -define icon:auto-resize=256 `
   -background white -transparent white `
   -compress zip $Root\..\build\icon.ico
 
-magick $Root\..\assets\icon\tome.png `
-  -resize 256x256 $Root\..\build\icon.png
+magick $Root\..\assets\icon\tome.1024.png `
+  -resize 256x256 $Root\..\build\icon.256.png
 
-magick $Root\..\assets\icon\tome.png `
+magick $Root\..\assets\icon\tome.1024.png `
+  -resize 512x512 $Root\..\build\icon.512.png
+
+magick $Root\..\assets\icon\tome.1024.png `
   -define icon:auto-resize=32,48,64,128,256 `
   -background white -transparent white `
   -compress zip $Root\..\assets\icon\tome.ico
