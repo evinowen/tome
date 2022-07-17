ed8da372691d4b56f61d30a052d661fcfbd444c0
Evin Owen <git@evinowen.net>
2022-07-16 17:26:58 -0700

    Additional ico updates to help Linux build workflow

--- scripts/generate.ico.from.svg.ps1
+++ scripts/generate.ico.from.svg.ps1
@@ -3,4 +3,9 @@ $Root = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 magick $Root\..\assets\icon\tome.png `
   -define icon:auto-resize=256 `
   -background white -transparent white `
+  -compress zip $Root\..\build\icon.ico
+
+magick $Root\..\assets\icon\tome.png `
+  -define icon:auto-resize=32,48,64,128,256 `
+  -background white -transparent white `
   -compress zip $Root\..\assets\icon\tome.ico
