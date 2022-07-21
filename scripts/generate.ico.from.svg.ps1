1dae5dadf971640f6d225456bd10215918e66cd3
Evin Owen <github@evinowen.net>
2022-07-20 20:24:17 -0700

    Feature: GitHub Actions Setup (#57)
    * Clean up GitHub Actions workflow config
    
    * Attempt fix for Tag/Release names
    
    * Disable steps for testing
    
    * Attempt fix for Tag/Release names
    
    * Attempt to set package version
    
    * Attempt fix or package_version set
    
    * Set artifact output name in package.json
    
    * Commas are essential in JSON
    
    * Attemp setup asset upload for workflow
    
    * Attempt workflow fix for PowerShell values
    
    * Fix for build tools path in workflow
    
    * Additional workflow fixes
    
    * Attempt fix for PowerShell build tools install
    
    * Adjustments to Start-Process for build tools install
    
    * Attempt fix for Start-Process parameters
    
    * Update workflow to test full process
    
    * Update workflow for build tools fix
    
    * Additional fix for build tools path
    
    * Correct build tools command line paramters
    
    * Attempt fix for build tools install
    
    * Add missing quotes for build tools install path
    
    * Test initial workflow for versioning validation
    
    * Configure version validation for workflow
    
    * Fix published package version calculation
    
    * Display tags for published package version calculation
    
    * Add fetch for tags
    
    * Set use create and upload release only on master
    
    * Fix workflow release conditionals
    
    * Attempt conditional fix again
    
    * Use single quote conditionals
    
    * Clean up workflow configuration
    
    * Add manual dispatch to workflow
    
    * Remove legacy upload step in workflow
    
    * Condence and test create release action
    
    * Update asset and release publishing process
    
    * Add build job requirement to publish job
    
    * Update artifact pathing for release publishing
    
    * Attempt path fix for release upload
    
    * Fix incorrect secret name
    
    * Attemp MacOS setup for build workflow
    
    * Attempt to skip broken OpenSSL test for MacOS
    
    * Attempt to skip broken tests for OpenSSL
    
    * Tests to skip use spaces and not commas
    
    * Update package.json to support cross-platform scripts
    
    * Add postinstall to support MacOS build
    
    * Update package.json to address MacOS build
    
    * Fix cli-truncate version to avoid package issue in build
    
    * Configure PACKAGE_VERSION for MacOS workflow
    
    * Fix PACKAGE_VERSION assignment for MacOS
    
    * Fix macOS upload artifact extension
    
    * Fix MacOS artifact path
    
    * Run end to end test of Windows + MacOS workflow
    
    * Update package-lock.json
    
    * Refactor version variables and validation
    
    * Update version script for Ubuntu
    
    * Add Ubuntu to workflow for Debian build
    
    * Add apt-get install for krb5 error on Ubuntu
    
    * Move krb5 fix to correct job
    
    * Update tome.ico and generation for Ubuntu/Debian build
    
    * Update ico generation for exact 256x256 requirement on Linux
    
    * Additional ico updates to help Linux build workflow
    
    * Create new png icon for linux build
    
    * Fix artifact names and paths
    
    * Update icon generation and assignment in package.json
    
    * Fix quoting issues and convert release step to Linux
    
    * Update scripts and start process to resolve dev/prod flag issue
    
    * Fix icon generation inconsistencies
    
    * Update to only publish a release when branch is master

--- scripts/generate.ico.from.svg.ps1
+++ scripts/generate.ico.from.svg.ps1
@@ -1,19 +1,29 @@
 $Root = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
-magick convert -background none -resize 16x16 $Root\..\assets\icon\tome.svg $Root\..\assets\icon\tome.16.png
-magick convert -background none -resize 32x32 $Root\..\assets\icon\tome.svg $Root\..\assets\icon\tome.32.png
-magick convert -background none -resize 64x64 $Root\..\assets\icon\tome.svg $Root\..\assets\icon\tome.64.png
-magick convert -background none -resize 128x128 $Root\..\assets\icon\tome.svg $Root\..\assets\icon\tome.128.png
-magick convert -background none -resize 256x256 $Root\..\assets\icon\tome.svg $Root\..\assets\icon\tome.256.png
-magick convert -background none -resize 512x512 $Root\..\assets\icon\tome.svg $Root\..\assets\icon\tome.512.png
-magick convert -background none -resize 1024x1024 $Root\..\assets\icon\tome.svg $Root\..\assets\icon\tome.1024.png
+magick convert -background none $Root\..\assets\icon\tome.svg $Root\..\assets\icon\tome.png
 
-magick convert -background transparent `
-  $Root\..\assets\icon\tome.16.png  `
-  $Root\..\assets\icon\tome.32.png  `
-  $Root\..\assets\icon\tome.64.png  `
-  $Root\..\assets\icon\tome.128.png  `
-  $Root\..\assets\icon\tome.256.png  `
-  $Root\..\assets\icon\tome.512.png  `
-  $Root\..\assets\icon\tome.1024.png  `
-  $Root\..\assets\icon\tome.ico
+magick convert -background none $Root\..\assets\icon\tome.svg -resize 16x16 $Root\..\assets\icon\tome.16.png
+magick convert -background none $Root\..\assets\icon\tome.svg -resize 32x32 $Root\..\assets\icon\tome.32.png
+magick convert -background none $Root\..\assets\icon\tome.svg -resize 64x64 $Root\..\assets\icon\tome.64.png
+magick convert -background none $Root\..\assets\icon\tome.svg -resize 128x128 $Root\..\assets\icon\tome.128.png
+magick convert -background none $Root\..\assets\icon\tome.svg -resize 256x256 $Root\..\assets\icon\tome.256.png
+magick convert -background none $Root\..\assets\icon\tome.svg -resize 512x512 $Root\..\assets\icon\tome.512.png
+magick convert -background none $Root\..\assets\icon\tome.svg -resize 1024x1024 $Root\..\assets\icon\tome.1024.png
+
+magick $Root\..\assets\icon\tome.svg `
+  -define icon:auto-resize=256 `
+  -background white -transparent white `
+  -compress zip $Root\..\build\icon.ico
+
+magick $Root\..\assets\icon\tome.svg `
+  -background white -transparent white `
+  -resize 256x256 $Root\..\build\icon.256.png
+
+magick $Root\..\assets\icon\tome.svg `
+  -background white -transparent white `
+  -resize 512x512 $Root\..\build\icon.512.png
+
+magick $Root\..\assets\icon\tome.svg `
+  -define icon:auto-resize=16,24,32,48,64,128,256 `
+  -background white -transparent white `
+  -compress zip $Root\..\build\icon.ico
