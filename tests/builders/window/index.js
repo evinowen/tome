60cb54717a3eaef0fc26a3202c3861715bb7af9b
Evin Owen <git@evinowen.net>
2022-08-15 11:21:06 -0700

    Update renderer search tests to address changes

--- tests/builders/window/index.js
+++ tests/builders/window/index.js
@@ -3,6 +3,7 @@ import disk from './disk'
 import dialog from './dialog'
 import file from './file'
 import git from './git'
+import metadata from './metadata'
 import path from './path'
 import ssl from './ssl'
 import window from './window'
@@ -17,6 +18,7 @@ export default function (callable) {
       ...clipboard,
       ...file,
       ...git,
+      ...metadata,
       ...path,
       ...ssl,
       ...window
