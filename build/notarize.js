08c96fabc0fa6452f69bbc97a9bfe237990aa5db
Evin Owen <git@evinowen.net>
2022-09-18 12:39:16 -0700

    Attempt update to workflow to increase verbosity of build output

--- build/notarize.js
+++ build/notarize.js
@@ -3,16 +3,23 @@ const { notarize } = require('electron-notarize')
 
 exports.default = async function notarizing (context) {
   const { electronPlatformName, appOutDir } = context
+  console.log('Notarize Platform Check', electronPlatformName, appOutDir)
+
   if (electronPlatformName !== 'darwin') {
+    console.log('Notarize Not Required', electronPlatformName)
     return
   }
 
   const appName = context.packager.appInfo.productFilename
 
-  return await notarize({
+  const config = {
     appBundleId: 'com.evinowen.tome',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_ID_PASSWORD
-  })
+  }
+
+  console.log('Notarize', config)
+
+  return await notarize(config)
 }
