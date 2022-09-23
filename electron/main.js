9a63b5269241e6245d61bf3555c3cc0e65694d12
Evin Owen <git@evinowen.net>
2022-09-23 11:15:33 -0700

    Increase logging of process start and main process events

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,9 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
-
+const log = require('electron-log')
 const path = require('path')
 const util = require('util')
+
+log.info('Main Process Start')
 
 const register = (win) => {
   ipcMain.removeAllListeners()
@@ -25,11 +27,13 @@ const createWindow = async () => {
   let development = false
   let frame = false
 
+  log.info('Building window for environment', process.env.NODE_ENV)
   if (process.env.NODE_ENV === 'development') {
     development = true
   }
 
   if (process.env.FRAME_WINDOW) {
+    log.info('Building window found FRAME_WINDOW flag, window set to build framed')
     frame = true
   }
 
@@ -63,20 +67,25 @@ const createWindow = async () => {
 
 try {
   module.exports = app.whenReady().then(async () => {
+    log.info('Application ready, calling to build window')
     await createWindow()
 
     app.onAsync('activate', async () => {
+      log.info('Process event: activate', process.platform)
       if (BrowserWindow.getAllWindows().length === 0) {
+        log.info('Process currently without window, calling to build window')
         await createWindow()
       }
     })
   })
 
   app.onAsync('window-all-closed', async () => {
+    log.info('Process event: window-all-closed', process.platform)
     if (process.platform !== 'darwin') {
+      log.info('Process non-darwin, calling to quit application')
       app.quit()
     }
   })
 } catch (error) {
-  console.error('Exiting due to fundamental error in main process', error)
+  log.error('Exiting due to fundamental error in main process', error)
 }
