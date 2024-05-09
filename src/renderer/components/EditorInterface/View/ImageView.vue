bfa1b0bf04842768ab91cb9023b064abdf0bc329
Evin Owen <git@evinowen.net>
2024-05-09 12:31:12 -0700

    Improved image panning and added controllable zoom

--- src/renderer/components/EditorInterface/View/ImageView.vue
+++ src/renderer/components/EditorInterface/View/ImageView.vue
@@ -1,7 +1,10 @@
 <template>
   <div
     ref="preview"
-    :class="[ 'image-preview', zoom ? 'image-preview-zoom' : '' ]"
+    :class="[
+      'image-preview',
+      { 'image-preview-zoom': zoom }
+    ]"
   >
     <button-icon
       v-if="hide"
@@ -14,10 +17,19 @@
       v-else
       ref="image"
       :src="file.path"
-      :class="[ 'preview', zoom ? 'preview-zoom' : '' ]"
-      draggable
-      @mouseup="mouseup"
-      @mousedown="mousedown"
+      :class="[
+        'preview',
+        { 'preview-zoom': zoom },
+        { 'preview-drag': dragging },
+      ]"
+      :style="zoom ? {
+        width: `${zoom_width}px`,
+        height: `${zoom_height}px`
+      } : {}"
+      @pointerup="mouseup"
+      @pointermove="mousemove"
+      @pointerdown="mousedown"
+      @wheel="mousewheel"
       @error="error"
     >
   </div>
@@ -35,7 +47,7 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref, watch } from 'vue'
+import { ref, watch } from 'vue'
 import { File } from '@/store'
 
 export interface Properties {
@@ -47,87 +59,113 @@ const properties = withDefaults(defineProps<Properties>(), {
 })
 
 const preview = ref<HTMLElement>(undefined)
+const image = ref<HTMLImageElement>()
 
 const hide = ref(false)
-const zoom = ref(false)
+const zoom = ref<number>(0)
+
+const zoom_offset_x = ref<number>(0)
+const zoom_offset_y = ref<number>(0)
+const zoom_width = ref<number>(0)
+const zoom_height = ref<number>(0)
 
 watch(() => properties.file, () => {
   hide.value = false
-  zoom.value = false
+  zoom.value = 0
 })
 
 function error () {
   hide.value = true
 }
 
-let dragable = false
-let dragging = false
-const mouseposition = {
-  x: 0,
-  y: 0,
-}
+const dragable = ref(false)
+const dragging = ref(false)
+const mouseposition_x = ref(0)
+const mouseposition_y = ref(0)
 
-onMounted(() => {
-  window.addEventListener('mousemove', mousemove)
-  window.addEventListener('mouseup', mousedragdone)
-  window.addEventListener('mouseleave', mousedragdone)
-})
+async function mouseup (event: PointerEvent) {
+  image.value.releasePointerCapture(event.pointerId)
+  dragable.value = false
 
-onUnmounted(() => {
-  window.removeEventListener('mousemove', mousemove)
-  window.removeEventListener('mouseup', mousedragdone)
-  window.removeEventListener('mouseleave', mousedragdone)
-})
-
-async function mouseup (event) {
-  dragable = false
-
-  if (dragging) {
-    mousedragdone(event)
+  if (dragging.value) {
+    dragging.value = false
     return
   }
 
-  const scroll_x = event.offsetX / event.target.offsetWidth
-  const scroll_y = event.offsetY / event.target.offsetHeight
+  zoom_offset_x.value = event.offsetX / image.value.offsetWidth
+  zoom_offset_y.value = event.offsetY / image.value.offsetHeight
 
-  zoom.value = !zoom.value
-
-  await nextTick()
-
-  const top = (scroll_y * event.target.offsetHeight) - (preview.value.offsetHeight * 0.5)
-  const left = (scroll_x * event.target.offsetWidth) - (preview.value.offsetWidth * 0.5)
-  const behavior = 'auto'
-
-  preview.value.scrollTo({ top, left, behavior })
+  zoom.value = zoom.value > 0 ? 0 : 100
 }
 
-async function mousemove (event: MouseEvent) {
-  if (dragable && event.buttons == 1) {
-    dragging = true
+function mousemove (event: PointerEvent) {
+  if (dragable.value) {
+    dragging.value = true
     event.preventDefault()
 
-    preview.value.scrollTop += mouseposition.y - event.screenY
-    preview.value.scrollLeft += mouseposition.x - event.screenX
+    preview.value.scrollTop += mouseposition_y.value - event.screenY
+    preview.value.scrollLeft += mouseposition_x.value - event.screenX
 
-    mouseposition.x = event.screenX
-    mouseposition.y = event.screenY
+    mouseposition_x.value = event.screenX
+    mouseposition_y.value = event.screenY
   }
 }
 
-async function mousedown (event: MouseEvent) {
-  dragable = true
-  mouseposition.x = event.screenX
-  mouseposition.y = event.screenY
+function mousedown (event: PointerEvent) {
+  image.value.setPointerCapture(event.pointerId)
+  dragable.value = true
+  mouseposition_x.value = event.screenX
+  mouseposition_y.value = event.screenY
 }
 
-async function mousedragdone (event: MouseEvent) {
-  dragging = false
+function mousewheel (event: WheelEvent) {
+  event.preventDefault()
+
+  if (event.deltaY === 0) {
+    return
+  }
+
+  const zoom_fast = event.ctrlKey
+
+  zoom_offset_x.value = (preview.value.scrollLeft + (preview.value.offsetWidth / 2)) / preview.value.scrollWidth
+  zoom_offset_y.value = (preview.value.scrollTop + (preview.value.offsetHeight / 2)) / preview.value.scrollHeight
+
+  if (zoom.value === 0) {
+    zoom.value = (image.value.offsetWidth / image.value.naturalWidth) * 100
+  }
+
+  let value = zoom.value - event.deltaY / (zoom_fast ? 10 : 50)
+
+  const zoom_width_percent = ((value * image.value.naturalWidth) / preview.value.offsetWidth)
+  const zoom_height_percent = ((value * image.value.naturalHeight) / preview.value.offsetHeight)
+  if ((zoom_width_percent <= 95) && (zoom_height_percent <= 95)) {
+    value = 0
+  }
+
+  zoom.value = value < 0 ? 0 : value
 }
+
+watch(zoom, async (value) => {
+  zoom_width.value = image.value.naturalWidth * (value / 100)
+  zoom_height.value = image.value.naturalHeight * (value / 100)
+
+  await nextTick()
+
+  const top = (zoom_offset_y.value * zoom_height.value) - (preview.value.offsetHeight * 0.5)
+  const left = (zoom_offset_x.value * zoom_width.value) - (preview.value.offsetWidth * 0.5)
+  const behavior = 'auto'
+
+  preview.value.scrollTo({ top, left, behavior })
+})
 
 defineExpose({
   error,
   hide,
   zoom,
+  dragable,
+  dragging,
+  mouseposition_x,
+  mouseposition_y,
 })
 </script>
 
@@ -164,6 +202,10 @@ defineExpose({
   max-width: unset;
   max-height: unset;
   cursor: zoom-out;
+}
+
+.preview-drag {
+  cursor: grabbing;
 }
 
 .failed {
